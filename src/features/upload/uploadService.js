75d977811f3c8b8f9b00d72b579f4f83c216f145
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-09-22 18:25:46 +0530

    made change in image upload

--- src/features/upload/uploadService.js
+++ src/features/upload/uploadService.js
@@ -4,7 +4,7 @@ import { base_url } from "../../utils/baseUrl";
 
 // Image upload function
 const uploadImg = async (data) => {
-  const response = await axios.post(`${base_url}upload/`, data, config);
+  const response = await axios.post(`${base_url}upload`, data, config);
   return response.data;
 };
 
